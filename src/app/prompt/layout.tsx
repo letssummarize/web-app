2bf29f2e25d4ce368a4154fce40acf6357c134d6
sabriAlshibani <sbryalshybany1@gmail.com>
2025-03-20 00:11:37 +0300

    fix: change position of breakpoint

--- src/app/prompt/layout.tsx
+++ src/app/prompt/layout.tsx
@@ -1,7 +1,7 @@
-import Container from '@/components/Container';
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar/Navbar';
-import Image from 'next/image';
+import Container from "@/components/Container";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image";
 
 export default function PromptLayout({
   children,
@@ -15,8 +15,8 @@ export default function PromptLayout({
         alt=""
         aria-hidden="true"
         className="absolute left-1/2 -translate-x-1/2 -z-0 pointer-events-none opacity-80
-           -top-[700px] sm:top-[-100px] md:top-[-300px] lg:top-[-200px] xl:top-[-600px]
-           max-sm:w-[200vw] max-sm:left-1/2 max-sm:-translate-x-[50%] w-full"
+   -top-[700px] max-sm:top-[-50px] md:top-[-300px] lg:top-[-400px] xl:top-[-600px]
+   "
         width={2000}
         height={1000}
       />
@@ -24,7 +24,7 @@ export default function PromptLayout({
       <div className="relative z-1 h-full flex flex-col space-y-[160px]">
         <Navbar />
         <Container>
-          <div className='flex flex-col gap-[80px]'>{children}</div>
+          <div className="flex flex-col gap-[80px]">{children}</div>
         </Container>
         <Footer />
       </div>
